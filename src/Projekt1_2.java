e34f31a1377f304d6c7a20833451b3b00d1d1c6b
Yeetburger535 <alfred.stencel.535@gmail.com>
2026-03-28 10:49:13 +0100

    zrobione zadanie 2

--- src/Projekt1_2.java
+++ src/Projekt1_2.java
@@ -2,5 +2,44 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Projekt1_2{
+    public static void main(String[] args) {
+        Scanner wejscie = new Scanner(System.in);
+        int n = 0;
+
+       while (true){
+           System.out.println("Podaj liczbę całkowitą (0 lub większą) do obliczenia silni: ");
+
+           if (wejscie.hasNextInt()){
+               n = wejscie.nextInt();
+
+               if(n >= 0){
+                   break;
+               }
+
+               else {
+                   System.out.println("Błąd! Wprowadzono liczbę ujemną");
+               }
+           }
+
+           else{
+               System.out.println("Błąd! Wprowadzono niepoprawne dane");
+
+               wejscie.next();
+           }
+       }
+
+       //liczenie silni
+        BigInteger silnia = BigInteger.ONE;
+
+       for (int i = 2; i <= n; i++){
+           silnia = silnia.multiply(BigInteger.valueOf(i));
+       }
+
+       System.out.println("Silnia z " + n + " wynosi:");
+       System.out.println(silnia);
+
+       wejscie.close();
+
+    }
 
 }
